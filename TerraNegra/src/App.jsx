6456bd742f5682adb37e42146630fb401c817213
John Di Donna <139842867+DiDonna29@users.noreply.github.com>
2023-12-20 19:39:45 -0400

    Pruebameste

--- TerraNegra/src/App.jsx
+++ TerraNegra/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>PruebameEste</h2>
+      <h2>PruebameEsteSapo</h2>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
